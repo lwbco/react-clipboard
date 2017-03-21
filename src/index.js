39e4848d0a2bfd58f60b2a734abaaf0ef7a68069
Leeward Bound <linked@users.noreply.github.com>
2017-03-21 15:53:35 -0700

    react 15
    https://github.com/reactjs/core-notes/issues/6

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 const React = require("react");
 const ReactDOM = require("react-dom");
-const assign = require("react/lib/Object.assign");
+const assign = require("react/lib/object-assign");
 
 function noop() {}
 
